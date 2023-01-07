77c7a865d53f1afa1755dd835be72794c69e3a0e
Venkata_Satwik_Potula_Computer_Science <venkata.cs20@bmsce.ac.in>
2023-01-07 05:22:49 +0000

    testing if git integration works

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,204 @@
+// library main;
+
+// import 'package:flutter/material.dart';
+// part "login.dart";
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       title: 'Named Routes Demo',
+//       // Start the app with the "/" named route. In this case, the app starts
+//       // on the FirstScreen widget.
+//       initialRoute: '/',
+//       routes: {
+//         // When navigating to the "/" route, build the FirstScreen widget.
+//         '/': (context) => const FirstScreen(),
+//         // When navigating to the "/second" route, build the SecondScreen widget.
+//         '/second': (context) => const SecondScreen(),
+//       },
+//     ),
+//   );
+// }
+
+// class FirstScreen extends StatelessWidget {
+//   const FirstScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('First Screen'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           // Within the `FirstScreen` widget
+//           onPressed: () {
+//             // Navigate to the second screen using a named route.
+//             Navigator.pushNamed(context, '/second');
+//           },
+//           child: const Text('Launch screen'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class SecondScreen extends StatelessWidget {
+//   const SecondScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Second Screen'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           // Within the SecondScreen widget
+//           onPressed: () {
+//             // Navigate back to the first screen by popping the current route
+//             // off the stack.
+//             Navigator.pop(context);
+//           },
+//           child: const Text('Go back!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// // Copyright 2020, the Flutter project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:window_size/window_size.dart';
 
-void main() => runApp(MyApp());
+import 'src/autofill.dart';
+import 'src/form_widgets.dart';
+import 'src/http/mock_client.dart';
+import 'src/sign_in_http.dart';
+import 'src/validation.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() {
+  setupWindow();
+  runApp(const FormApp());
+}
+
+const double windowWidth = 480;
+const double windowHeight = 854;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Form Samples');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(Rect.fromCenter(
+        center: screen!.frame.center,
+        width: windowWidth,
+        height: windowHeight,
+      ));
+    });
+  }
+}
+
+final demos = [
+  Demo(
+    name: 'Sign in with HTTP',
+    route: 'signin_http',
+    builder: (context) => SignInHttpDemo(
+      // This sample uses a mock HTTP client.
+      httpClient: mockClient,
+    ),
+  ),
+  Demo(
+    name: 'Autofill',
+    route: 'autofill',
+    builder: (context) => const AutofillDemo(),
+  ),
+  Demo(
+    name: 'Form widgets',
+    route: 'form_widgets',
+    builder: (context) => const FormWidgetsDemo(),
+  ),
+  Demo(
+    name: 'Validation',
+    route: 'validation',
+    builder: (context) => const FormValidationDemo(),
+  ),
+];
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+      routes: [
+        for (final demo in demos)
+          GoRoute(
+            path: demo.route,
+            builder: (context, state) => demo.builder(context),
+          ),
+      ],
+    ),
+  ],
+);
+
+class FormApp extends StatelessWidget {
+  const FormApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      title: 'Form Samples',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      routerConfig: router,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: const Text('Form Samples'),
       ),
-      body: Center(
-        child: Text(
-          'Welcome to our app',
-        ),
+      body: ListView(
+        children: [...demos.map((d) => DemoTile(demo: d))],
       ),
     );
   }
+}
+
+class DemoTile extends StatelessWidget {
+  final Demo? demo;
+
+  const DemoTile({this.demo, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(demo!.name),
+      onTap: () {
+        context.go('/${demo!.route}');
+      },
+    );
+  }
+}
+
+class Demo {
+  final String name;
+  final String route;
+  final WidgetBuilder builder;
+
+  const Demo({required this.name, required this.route, required this.builder});
 }
